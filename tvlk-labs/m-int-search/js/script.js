0972496ab97a359f54e94d507a862e65c2193828
arieare <arie.a.re@gmail.com>
2016-02-11 10:20:00 +0700

    increasing delay between interaction to gently guide user's attention

--- tvlk-labs/m-int-search/js/script.js
+++ tvlk-labs/m-int-search/js/script.js
@@ -11,7 +11,7 @@ $(function() {
 		if ($("#bullet-number-2").html() != "✓") {
 			setTimeout(function(){
 				$('ul.tabs').tabs('select_tab', 'rtn-column');
-			},500);
+			},1500);
 		}
 		else {
 			
@@ -36,7 +36,7 @@ $(function() {
 		if ($("#bullet-number-1").html() != "✓") {
 			setTimeout(function(){
 				$('ul.tabs').tabs('select_tab', 'dpt-column');
-			},500);
+			},1500);
 		}
 		else {
 			
